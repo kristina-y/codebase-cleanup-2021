4c5bb4a880d804d7febbf53d4c74fa2fc8c7540a
Kristina Yarovinsky <ky226@georgetown.edu>
2021-04-05 10:24:38 -0400

    Matching Product Function in Shopping Cart

--- test/shopping_test.py
+++ test/shopping_test.py
@@ -1,7 +1,8 @@
 
 # import function
-
-from app.shopping import format_usd
+import os
+from pandas import read_csv
+from app.shopping import format_usd, lookup_product
 
 # test the function
 
@@ -10,5 +11,19 @@ def test_format_usd():
     assert format_usd(2.2222) == "$2.22"
 
 
-# once we have some tests...
+mock_products_filepath = os.path.join(os.path.dirname(__file__), "mock_data", "mock_products.csv")
+mock_products_df = read_csv(mock_products_filepath)
+mock_products = mock_products_df.to_dict("records")
 
+def test_lookups():
+    valid_result = lookup_product("8", mock_products)
+    assert valid_result == {
+        'aisle': 'Aisle C',
+        'department': 'snacks',
+        'id': '8',
+        'name': 'Product 8',
+        'price': 10.0
+    }
+
+    invalid_result = lookup_product("888880", mock_products)
+    assert invalid_results == None
