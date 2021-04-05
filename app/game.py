5c3a1710209d4fe14a5f748ed30e642b52ff01f9
kristina-y <77757208+kristina-y@users.noreply.github.com>
2021-04-04 20:53:33 -0400

    Simple (#1)
    * Refactored valid options in our game
    
    * Refactored valid options in our game and started simplifications
    
    * Started to reduce complexity
    
    * format to USD function
    
    * Added Doc String

--- app/game.py
+++ app/game.py
@@ -1,13 +1,15 @@
-
+ 
 from random import choice
 
 #
 # USER SELECTION
 #
 
+VALID_OPTIONS = ["rock", "paper", "scissors"]
+
 u = input("Please choose one of 'Rock', 'Paper', or 'Scissors': ").lower()
 print("USER CHOICE:", u)
-if u not in ["rock", "paper", "scissors"]:
+if u not in VALID_OPTIONS:
     print("OOPS, TRY AGAIN")
     exit()
 
@@ -15,30 +17,17 @@ if u not in ["rock", "paper", "scissors"]:
 # COMPUTER SELECTION
 #
 
-c = choice(["rock", "paper", "scissors"])
+c = choice(VALID_OPTIONS)
 print("COMPUTER CHOICE:", c)
 
 #
 # DETERMINATION OF WINNER
 #
-
-if u == "rock" and c == "rock":
+if u == c:
     print("It's a tie!")
-elif u == "rock" and c == "paper":
-    print("The computer wins")
-elif u == "rock" and c == "scissors":
-    print("The user wins")
 
-elif u == "paper" and c == "rock":
+elif (u == "rock" and c == "paper") or (u == "paper" and c == "scissors") or (u == "scissors" and c == "rock"):
     print("The computer wins")
-elif u == "paper" and c == "paper":
-    print("It's a tie!")
-elif u == "paper" and c == "scissors":
-    print("The user wins")
 
-elif u == "scissors" and c == "rock":
-    print("The computer wins")
-elif u == "scissors" and c == "paper":
+elif (u == "rock" and c == "scissors") or (u == "paper" and c == "rock") or (u == "scissors" and c == "paper"):
     print("The user wins")
-elif u == "scissors" and c == "scissors":
-    print("It's a tie!")
