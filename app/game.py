6f2e19273fc139dd2e8cd5efb7adb39dd38e611b
Kristina Yarovinsky <ky226@georgetown.edu>
2021-04-04 22:46:18 -0400

    Started game.py winner logic test
    Updated the logic using dictionary provided by Prof. Rossetti, created the baseline for the test in game_test

--- app/game.py
+++ app/game.py
@@ -1,33 +1,67 @@
  
 from random import choice
 
-#
-# USER SELECTION
-#
-
 VALID_OPTIONS = ["rock", "paper", "scissors"]
 
-u = input("Please choose one of 'Rock', 'Paper', or 'Scissors': ").lower()
-print("USER CHOICE:", u)
-if u not in VALID_OPTIONS:
-    print("OOPS, TRY AGAIN")
-    exit()
+def determine_winner(choice1, choice2):
+    """
+    This function determines the winner of a rock, paper, scissors game, given 2 inputs
+    
+    Params:
+        Choice1 and Choice2 are string that are within the VALID_OPTIONS array
 
-#
-# COMPUTER SELECTION
-#
+    Examples:
+        determine_winner(rock, paper)
+        determine_winner(scissors, rock)
+    """
+    winners = {
+        "rock":{
+            "rock": None,
+            "paper": "paper",
+            "scissors": "rock",
+        },
+        "paper":{
+            "rock": "paper",
+            "paper": None,
+            "scissors": "scissors",
+        },
+        "scissors":{
+            "rock": "rock",
+            "paper": "scissors",
+            "scissors": None,
+        },
+    }
+    winning_choice = winners[choice1][choice2]
+    return winning_choice
 
-c = choice(VALID_OPTIONS)
-print("COMPUTER CHOICE:", c)
 
-#
-# DETERMINATION OF WINNER
-#
-if u == c:
-    print("It's a tie!")
+if __name__ == "__main__":
+    #
+    # USER SELECTION
+    #
 
-elif (u == "rock" and c == "paper") or (u == "paper" and c == "scissors") or (u == "scissors" and c == "rock"):
-    print("The computer wins")
 
-elif (u == "rock" and c == "scissors") or (u == "paper" and c == "rock") or (u == "scissors" and c == "paper"):
-    print("The user wins")
+    u = input("Please choose one of 'Rock', 'Paper', or 'Scissors': ").lower()
+    print("USER CHOICE:", u)
+    if u not in VALID_OPTIONS:
+        print("OOPS, TRY AGAIN")
+        exit()
+
+    #
+    # COMPUTER SELECTION
+    #
+
+    c = choice(VALID_OPTIONS)
+    print("COMPUTER CHOICE:", c)
+
+    #
+    # DETERMINATION OF WINNER
+    #
+    winner = determine_winner(u,c)
+
+    if winner == u:
+        print("You won")
+    elif winner == c:
+        print("Computer won")
+    elif winner == NONE:
+        print("You tied with the computer")
