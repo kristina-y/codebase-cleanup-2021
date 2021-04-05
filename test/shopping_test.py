2ff6ebb0ba52eb7299d50fb4a54847fb4e83a2c8
Kristina Yarovinsky <ky226@georgetown.edu>
2021-04-04 22:01:53 -0400

    Testing format_USD function in shopping.py

--- test/shopping_test.py
+++ test/shopping_test.py
@@ -1,4 +1,14 @@
 
-# TODO: import some code
+# import function
 
-# TODO: test the code
+from app.shopping import format_usd
+
+# test the function
+
+def test_format_usd():
+    assert format_usd(9.5) == "$9.50"
+    assert format_usd(2.2222) == "$2.22"
+
+
+# once we have some tests...
+
